32ac0503efd2305479ef2f1444ebb11984702b47
DinoTooIs <44649239+DinoTooIs@users.noreply.github.com>
2018-11-05 18:31:50 -0500

    src/scan - Restructure preferred cipher scan

--- sslscan.py
+++ sslscan.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import sys
 
-if sys.version_info[0] < 3:
-    print("[!] Please use python3 to have multithread support")
+if sys.version_info[0] != 2:
+    print("[!] This program is only compatible with python 2")
     exit(-1)
 
 from sslscan import ui
